e35d270073deeb1f7c7f401c6652ee1821cb1784
clballes <claramballester@gmail.com>
2023-05-23 15:10:18 +0200

    master

---
+++ obj/builtins/exit.d
@@ -0,0 +1,2 @@
+obj/builtins/exit.o: src/builtins/exit.c inc/../inc/minishell.h \
+  inc/../libft/libft.h inc/../inc/builtins.h
---
+++ obj/builtins/unset.d
@@ -0,0 +1,2 @@
+obj/builtins/unset.o: src/builtins/unset.c inc/../inc/minishell.h \
+  inc/../libft/libft.h inc/../inc/builtins.h

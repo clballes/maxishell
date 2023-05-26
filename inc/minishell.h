f6982a70577e7a917ba10e47f82dbb9685bd3234
clballes <claramballester@gmail.com>
2023-05-26 16:40:11 +0200

    doing pipes

--- inc/minishell.h
+++ inc/minishell.h
@@ -96,9 +96,6 @@ int		search_path(t_all *all);
 void		fork_function(t_all *all, char *new_path);
 void	free_lists_and_line(t_all *all);
 
-<<<<<<< HEAD
 // pipes
 void	pipes(t_all *all);
-=======
->>>>>>> heredoc
 #endif

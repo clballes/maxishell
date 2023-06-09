ae52cd866e189b2caae39777467a5cfc68ac7a82
clballes <claramballester@gmail.com>
2023-06-09 13:22:27 +0200

    redi input done

--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -21,12 +21,12 @@ int	write_err(t_all *all, int j)
 	all->exit = 1;
 	if (j == 1)
 		ft_putstrshell_fd("minishell: &: Permission denied", 2, all, 2);
-	else if (j ==3)
-		
+	else if (j == 2)
+		ft_putstrshell_fd("minishell: &: No such file or directory", 2, all, 2);
 	else
 		ft_putstrshell_fd("minishell: &: Is a directory", 2, all, 2);
 	write(2, "\n", 1);
-	return (1);
+	exit(all->exit);
 }
 
 int	access_path(char *filename, t_all *all)
@@ -40,19 +40,18 @@ int	access_path(char *filename, t_all *all)
 			if (access(filename, W_OK) == 0)
 				return (0); // si existe y tenemos acceso, haremos el open
 			else
-			{
-				write_err(all, 0); // is a directory
-				exit(all->exit);
-			}
+				write_err(all, 1); // file we dont have permision
 		}
 		else
-		{
 			write_err(all, 0); // is a directory
-			exit(all->exit);
-		}
 	}
 	else
+	{
+		if (all->node->redir->type == INPUT) // file not existent so not doing anytign in input redirection
+			write_err(all, 2);
 		return (2);// necesitamos crear el fichero
+	}
+	return (0);
 }
 
 int	redir_output(t_all *all, int type)
@@ -69,14 +68,13 @@ int	redir_output(t_all *all, int type)
 	if (fd == -1)
 	{
 		perror("open");
-		return 1;
+		return (1);
 	}
 	if (dup2(fd, STDOUT_FILENO) == -1)
 	{
 		perror("dup2");
-		return 1;
+		return (1);
 	}
-	//cuando hay mas d un nodo ejecutar comadno al final
 	if (all->node->redir->next == NULL)
 		exec_cmd(all, all->node);
 	// if (dup2(stdout_copy, STDOUT_FILENO) == -1) esto lo hemos de hacer si no es forkabloe TODO
@@ -94,29 +92,21 @@ int	redir_output(t_all *all, int type)
 int	redir_input(t_all *all)
 {
 	int fd;
-		printf("AAAAAAAAAA\n");
 
-	fd = open(all->node->redir->file_name, O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Failed to open the input file.\n");
-		return 1;
-	}
-	// Redirect the standard input to the input file
+    fd = open(all->node->redir->file_name, O_RDONLY);
+    if (fd == -1) {
+        printf("Failed to open the input file\n");
+        return 1;
+    }
 	if (dup2(fd, STDIN_FILENO) == -1)
 	{
-		printf("Failed to redirect standard input.\n");
-		return 1;
+		perror("dup2");
+		return (1);
 	}
-	
-	// Read input from the redirected standard input
-	// char buffer[256];
-	// fgets(buffer, sizeof(buffer), stdin);
-	// printf("Input: %s", buffer);
-	
-	// Close the input file
-	close(fd);
-	return 0;
+	exec_cmd(all, all->node);
+    // Close the input file
+    close(fd);
+    return (0);
 }
 
 int	redir_loop(t_cmd *node, t_all *all)
@@ -126,16 +116,13 @@ int	redir_loop(t_cmd *node, t_all *all)
 	access = 0;
 	while (node->redir)
 	{
-		printf("el redir type es %d\n", node->redir->type);
 		access = access_path(node->redir->file_name, all);
-		printf("el redir type es %d\n", access);
 		if (access != 1)
 		{
 			if (access == 2 && node->redir->type == INPUT)
 				write_err(all, 3);
 			if (access == 2 && (node->redir->type != INPUT || node->redir->type != HEREDOC)) // fer el open idiferent de que sigui
 			{
-				printf("entres?\n");
 				open(node->redir->file_name, O_WRONLY | O_CREAT , 0644);
 				redir_output(all, 0);
 			}
@@ -144,10 +131,7 @@ int	redir_loop(t_cmd *node, t_all *all)
 				if (node->redir->type == OUTPUT_TRUNCATED || node->redir->type == OUTPUT_APPEND)
 					redir_output(all, all->node->redir->type);
 				if (node->redir->type == INPUT)
-				{
-					printf("entru\n");
 					redir_input(all);
-				}
 				if (node->redir->type == HEREDOC)
 					printf("hola5");
 			}

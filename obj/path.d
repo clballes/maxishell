f70988d02f41edb360e52ad3e6fabd4cad055a86
clballes <claramballester@gmail.com>
2023-06-21 12:23:02 +0200

    shlvl done mpanic working on status

--- obj/path.d
+++
@@ -1,9 +0,0 @@
-obj/path.o: src/path.c src/../inc/minishell.h inc/../libft/libft.h \
-  /Users/clara/.brew/opt/readline/include/readline/readline.h \
-  /Users/clara/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/clara/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/clara/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/clara/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/clara/.brew/opt/readline/include/readline/tilde.h \
-  /Users/clara/.brew/opt/readline/include/readline/history.h \
-  src/../inc/builtins.h

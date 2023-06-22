79f953013b4e179f58aa5e13cfb609c16894d6f0
albagg3 <garciagonzalezalba@gmail.com>
2023-06-22 16:37:01 +0200

    exit normi a revisar

--- obj/path.d
+++
@@ -1,9 +0,0 @@
-obj/path.o: src/path.c src/../inc/minishell.h inc/../libft/libft.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/readline.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/tilde.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/history.h \
-  src/../inc/builtins.h
--- obj/utils_minishell.d
+++
@@ -1,9 +0,0 @@
-obj/utils_minishell.o: src/utils_minishell.c src/../inc/minishell.h \
-  inc/../libft/libft.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/readline.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/tilde.h \
-  /Users/albagarc/.brew/opt/readline/include/readline/history.h
